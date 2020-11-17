5b4588a5cf2207d2581eaf3524b279cce2177f47
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-11-17 17:26:52 +0600

    final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function clickHandleProductChange(product,increase){
     const productInput = document.getElementById(product+"Count");
-    const productCount = parseInt(productInput.value);
+    const productCount = getInputValue(product);
     let productNewCount = productCount;
     if (increase == true){
         productNewCount = productCount + 1;
@@ -16,4 +16,23 @@ function clickHandleProductChange(product,increase){
         productTotal = productNewCount * 1219;
     }
     document.getElementById(product + "Price").innerText= productTotal;
+    calculateTotal();
+}
+
+function calculateTotal(){
+    const phoneCount = getInputValue("phone");
+    const caseCount = getInputValue("case");
+    
+    const total = phoneCount * 1219 + caseCount * 59;
+    document.getElementById("subTotal").innerText = "$"+ total;
+    const tax = Math.ceil(total * 0.1);
+    document.getElementById("tax").innerText = "$" +tax;
+    const grandTotal = total + tax;
+    document.getElementById("totalPrice").innerText = "$"+ grandTotal;
+}
+
+function getInputValue(product){
+    const productInput = document.getElementById(product+"Count");
+    const productCount = parseInt(productInput.value);
+    return productCount;
 }
